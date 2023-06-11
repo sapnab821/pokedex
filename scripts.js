ccb376647a8f0955411a8b0d99d2e2cc8dd92535
Sapna <sapnabolikal@gmail.com>
2023-06-11 17:07:45 -0400

    task 1.1 revised

--- scripts.js
+++
@@ -1,5 +0,0 @@
-let pokemonList = [
-    {name: 'Bounsweet', height: 0.3, type: 'grass'},
-    {name: 'Nosepass', height: 1.0, type: 'rock'}, 
-    {name: 'Trevenant', height: 1.5, type: ['ghost', 'grass']}
-];
