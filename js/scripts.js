6234ef169ddc72ed8c3790b726eb878c0fe92080
Sapna <sapnabolikal@gmail.com>
2023-06-12 03:28:32 -0400

    changes for task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-alert('Hello world');
+let pokemonList = [
+    {
+        name: 'Bounsweet', 
+        height: 0.3, 
+        type: 'grass'
+    },
+    {
+        name: 'Nosepass', 
+        height: 1.0, 
+        type: 'rock'
+    }, 
+    {
+        name: 'Trevenant', 
+        height: 1.5, 
+        type: ['ghost', 'grass']
+    }
+];
 
-let favoriteFood= 'Pizza';
-
-document.write(favoriteFood);
