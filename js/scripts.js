0846a52a515ad1c15ef3465f4b01f34683e7c819
Sapna <sapnabolikal@gmail.com>
2023-06-14 12:45:17 -0400

    1.5 IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
-let pokemonList = [
-    {
-        name: 'Bounsweet', 
-        height: 0.3, 
-        type: 'grass'
-    },
-    {
-        name: 'Nosepass', 
-        height: 1.0, 
-        type: 'rock'
-    }, 
-    {
-        name: 'Trevenant', 
-        height: 1.5, 
-        type: ['ghost', 'grass']
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bounsweet',
+            height: 0.3,
+            type: 'grass'
+        },
+        {
+            name: 'Nosepass',
+            height: 1.0,
+            type: 'rock'
+        },
+        {
+            name: 'Trevenant',
+            height: 1.5,
+            type: ['ghost', 'grass']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
 
 /*for (let i=0; i< pokemonList.length; i++) {
     document.write(pokemonList[i].name + ' is height ' + pokemonList[i].height+'m and type ' + pokemonList[i].type + '. ');
@@ -25,9 +41,11 @@ let pokemonList = [
     
 } */
 
-
-pokemonList.forEach(function(user) {
+pokemonRepository.getAll().forEach(function(user) {
     document.write(user.name + ' is height ' + user.height + 'm and type ' + user.type + '. ');
+    if (user.height < 0.5) {
+        document.write('Wow that is small! ');
+    }
     document.write('\n');
 
 });
